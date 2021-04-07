55a712f0b7e5de0dd4abc53fd98c0d8c21c8d0ff
Artizazel <43548024+Artizazel@users.noreply.github.com>
2021-04-07 10:13:48 +0100

    Initial commit

---
+++ Tinky Tanks/Assets/Scripts/ProjectileHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileHandler : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(500 * transform.up);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tinky Tanks/Assets/Scripts/TankControls.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControls : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody;
+    public GameObject cannon;
+    public GameObject cannonEnd;
+    public GameObject projectile;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        basicMovement();
+        cannonAim();
+        shoot();
+
+    }
+
+    void basicMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(new Vector2(-100, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(new Vector2(100, 0));
+        }
+
+        rigidbody.velocity = new Vector2(0, 0);
+    }
+
+    void cannonAim()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            cannon.transform.Rotate(new Vector3(0, 0, 0.1f));
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            cannon.transform.Rotate(new Vector3(0, 0, -0.1f));
+        }
+    }
+
+    void shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectile, cannon.transform.position, cannon.transform.rotation);
+        }
+    }
+
+}
